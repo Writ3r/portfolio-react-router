3351bfd1c103977cba60b5c3060f79093c6c2a64
Lucas Wing <lucas.wing@aol.com>
2025-04-07 19:18:11 -0400

    got carousel mostly working

---
+++ app/components/ui/Carousel.tsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+import {
+  Navbar,
+  Carousel,
+  CarouselItem,
+  Button,
+  CarouselCaption,
+} from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
+
+import video from "./Letter.webm";
+
+interface VideoInput {
+  videoUrl: string;
+}
+
+const VideoBackground = (props: VideoInput) => {
+  return (
+    <video
+      className="video-intro"
+      autoPlay
+      loop
+      muted
+      style={{ maxWidth: "none" }}
+    >
+      <source src={props.videoUrl} type="video/mp4" />
+    </video>
+  );
+};
+
+interface CarouselCenterInnerInput {
+  saying: string;
+  code: string;
+  explination: string;
+}
+
+const CarouselCenterInner = (props: CarouselCenterInnerInput) => {
+  return (
+    <div className="mask rgba-black-light d-flex justify-content-center align-items-center">
+      <div className="text-center white-text mx-5" style={{ color: "white" }}>
+        <h1 className="mb-4">
+          <strong>{props.saying}</strong>
+        </h1>
+        <p className="animated pulse infinite">
+          <strong>{props.code}</strong>
+        </p>
+        <p className="mb-4 d-none d-md-block">
+          <strong>{props.explination}</strong>
+        </p>
+        <Button
+          variant="outline-light"
+          href="https://github.com/Writ3r/portfolio"
+          target="_blank"
+          rel="noreferrer"
+        >
+          SOURCE CODE
+          <FontAwesomeIcon
+            className="ml-2 animate__animated animate__swing animate__infinite"
+            icon={faGraduationCap}
+            style={{ color: "white" }}
+          />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export function PortfolioCarousel() {
+  return (
+    <Carousel className="carouselPage">
+      <CarouselItem>
+        <div className="view">
+          <VideoBackground videoUrl={video}></VideoBackground>
+          <CarouselCenterInner
+            saying="Keep It Simple, Silly"
+            code="isEven = (x % 2 == 0)"
+            explination="Code does not care about complexity, but the maintainers do."
+          />
+        </div>
+      </CarouselItem>
+    </Carousel>
+  );
+}
--- app/home/welcome.tsx
+++ app/home/welcome.tsx
@@ -1,12 +1,15 @@
 import { NavBar } from "~/components/ui/NavBar";
 import { Footer } from "~/components/ui/Footer";
 import { MainContent } from "~/components/ui/MainContent";
+import { PortfolioCarousel } from "~/components/ui/Carousel";
 
 export function Welcome({ message }: { message: string }) {
   return (
     <>
+      {/* makes the footer stuck down to page bottom when coupled with main's flex grow */}
       <div className="d-flex flex-column min-vh-100">
-        <NavBar></NavBar>
+        <NavBar />
+        <PortfolioCarousel />
         <main
           style={{ backgroundColor: "rgb(79, 95, 128)" }}
           className="flex items-center justify-center pt-16 pb-4 flex-grow-1"
