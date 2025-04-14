57aeb1b7a5a8c32ae01a9298615088300a63edc0
Lucas Wing <lucas.wing@aol.com>
2025-04-13 20:02:48 -0400

    cleaned up footer

--- app/components/ui/footer/Footer.tsx
+++ app/components/ui/footer/Footer.tsx
@@ -1,50 +1,52 @@
-import { Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
 
 export function Footer() {
   return (
-    <footer
-      className="text-center font-small mt-4 page-footer"
-      style={{ backgroundColor: "#1c2331" }}
-    >
+    <footer className="text-center font-small mt-4 page-footer">
       <div className="pt-4">
-        <Button variant="outline-light" className="">
+        <Button variant="outline-light">
           COVER LETTER
           <FontAwesomeIcon
-            className="ml-2 animate__animated animate__swing animate__infinite"
+            className="ml-2 fa-white-icon animate__animated animate__swing animate__infinite"
             icon={faDownload}
-            style={{ color: "white" }}
           />
         </Button>
-        <Button variant="outline-light" className="">
+        <Button variant="outline-light">
           COVER LETTER
           <FontAwesomeIcon
-            className="ml-2 animate__animated animate__swing animate__infinite"
+            className="ml-2 fa-white-icon animate__animated animate__swing animate__infinite"
             icon={faDownload}
-            style={{ color: "white" }}
           />
         </Button>
       </div>
       <hr className="my-4" />
       <div className="pb-4">
-        <FontAwesomeIcon
-          className="mr-3"
-          icon={faGithub}
-          style={{ color: "white" }}
-        />
-        <FontAwesomeIcon
-          className="mr-3"
-          icon={faLinkedin}
-          style={{ color: "white" }}
-        />
+        <a href="https://github.com/Writ3r" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon className="mr-3 fa-white-icon" icon={faGithub} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/lucas-wing-314894104/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon className="mr-3 fa-white-icon" icon={faLinkedin} />
+        </a>
       </div>
       <div className="footer-copyright py-3">
-        © {new Date().getFullYear()} Copyright:{" "}
-        <a href="https://lucaswing.com" target="_blank" rel="noreferrer">
-          lucaswing.com
-        </a>
+        <span>
+          © {new Date().getFullYear()} Copyright:
+          <a
+            href="https://lucaswing.com"
+            target="_blank"
+            rel="noreferrer"
+            className="ml-1"
+          >
+            lucaswing.com
+          </a>
+        </span>
       </div>
     </footer>
   );
